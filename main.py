bef9be8692f440bd4873274463d0a45c4d44e9a4
ankiwork-lgtm <anki.work@gmail.com>
2026-01-30 10:46:58 +0530

    langchain search commits

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_ollama import ChatOllama
-
-
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+#from langchain_openai import ChatOpenAI
+#from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 load_dotenv()
 
+
+#tavily = TavilyClient()
+
+
+@tool
+def search(query: str) -> str:
+    """
+    tool that searches over internet
+    Args:
+        query: the query to search for
+    Returns:
+        The search result
+    """
+
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
+
+llm = ChatOllama(model="llama3.2")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
+
 def main():
-    #print("Hello from langchain-course!")
-    information = """
-Narendra Damodardas Modi[a] (born 17 September 1950) is an Indian politician who has served as the prime minister of India since 2014. Modi was the chief minister of Gujarat from 2001 to 2014 and is the member of parliament (MP) for Varanasi. He is a member of the Bharatiya Janata Party (BJP) and of the Rashtriya Swayamsevak Sangh (RSS), a right-wing Hindutva paramilitary volunteer organisation. He is the longest-serving prime minister outside the Indian National Congress.
+    print("Hello from langchain course from Ankit")
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the Delhi NCR area on linkedin and list their details")})
+    print(result["messages"][-1].content)   
 
-Modi was born and raised in Vadnagar, Bombay State (present-day Gujarat), where he completed his secondary education. He was introduced to the RSS at the age of eight, becoming a full-time worker for the organisation in Gujarat in 1971. The RSS assigned him to the BJP in 1985, and he rose through the party hierarchy, becoming general secretary in 1998.[b] In 2001, Modi was appointed chief minister of Gujarat and elected to the legislative assembly soon after. His administration is considered complicit in the 2002 Gujarat violence[c] and has been criticised for its management of the crisis. According to official records, a little over 1,000 people were killed, three-quarters of whom were Muslim; independent sources estimated 2,000 deaths, mostly Muslim.[4] A Special Investigation Team appointed by the Supreme Court of India in 2012 found no evidence to initiate prosecution proceedings against him, causing widespread anger and disbelief among the country's Muslim communities.[d] While his policies as chief minister were credited for encouraging economic growth, his administration was criticised for failing to significantly improve health, poverty and education indices in the state.[e]
-"""
-
-    summary_template = """
-Given the information {information} about a person I want you to create:
- 1. A short summary
- 2. Two interesting facts about that person
-"""
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    )
-
-    llm = ChatOllama(
-         model= "llama3.2",
-         temperature= 0,
-    )
-    chain = summary_prompt_template | llm
-    response = chain.invoke(input={"information": information})
-    print(response.content)
 
 if __name__ == "__main__":
     main()
