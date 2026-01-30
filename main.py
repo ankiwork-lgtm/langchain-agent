1b907378e22c024446226b565dfec5da2aa16a94
ankiwork-lgtm <anki.work@gmail.com>
2026-01-30 15:45:31 +0530

    LG first commit

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
-from langchain_ollama import ChatOllama
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
-#from langchain_openai import ChatOpenAI
-#from tavily import TavilyClient
-from langchain_tavily import TavilySearch
+
 load_dotenv()
 
+#from langchain import hub
+#import langchainhub as hub
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch 
 
-#tavily = TavilyClient()
-
-
-@tool
-def search(query: str) -> str:
-    """
-    tool that searches over internet
-    Args:
-        query: the query to search for
-    Returns:
-        The search result
-    """
-
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
-
-llm = ChatOllama(model="llama3.2")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+llm = ChatOllama(model="llama3.2")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm = llm,
+    tools = tools,
+    prompt=react_prompt,
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
 
 def main():
-    print("Hello from langchain course from Ankit")
-    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the Delhi NCR area on linkedin and list their details")})
-    print(result["messages"][-1].content)   
+
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the Noida area on linkedin and list their details"
+        }
+    )
+    print("result")
 
 
 if __name__ == "__main__":
