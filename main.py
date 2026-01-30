3aad67cea2161f3aaa74859644366fcefa3e4f1d
ankiwork-lgtm <anki.work@gmail.com>
2026-01-30 22:36:33 +0530

    added structured formatting

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# from langchain import hub
-# import langchainhub as hub
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+#from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.runnables import RunnableLambda
 from langchain_core.prompts import PromptTemplate
 
@@ -20,11 +18,12 @@ from schemas import AgentResponse
 tools = [TavilySearch()]
 llm = ChatOllama(model="llama3.2")
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+#output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt_with_format_inst = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"]
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 
 agent = create_react_agent(
@@ -34,10 +33,10 @@ agent = create_react_agent(
 )
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+#parse_output = RunnableLambda(lambda x: output_parser.parse(x))
 
 
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
